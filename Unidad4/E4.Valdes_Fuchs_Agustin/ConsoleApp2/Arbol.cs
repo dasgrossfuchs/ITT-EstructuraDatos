67e5935260b12ce2ef762128c5b3d069c60f8a00
dasgrossfuchs <agustinabta@gmail.com>
2018-11-11 19:20:27 -0800

    dunnp

--- Unidad4/E4.Valdes_Fuchs_Agustin/ConsoleApp2/Arbol.cs
+++ Unidad4/E4.Valdes_Fuchs_Agustin/ConsoleApp2/Arbol.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp2
 {
     class Arbol
     {
+        public string path = "";
         public int Contador = 0;
         public Nodo top;
         public Arbol() { top = null; }
@@ -24,57 +25,57 @@ namespace ConsoleApp2
         }
         public void Agregar(  Nodo Y,string L1,string L2, string L3, string L4, string R1, string R2, string R3, string R4,string daddy)
         {// Y es el nodo al que se agregara el valor
-            //L y R son los valores
-            //daddy es la el valor guia del padre
-            
-                
-            
-                if (Y == null) { return;}
+         //L y R son los valores
+         //daddy es la el valor guia del padre
+
+
+
+            if (Y == null) { }
+            else
+            {
                 if (Y.Valor == daddy)
                 {
                     return;
                 }
                 //#Solo los chicos de los 90's recordaran haber hecho 8 metodos recursivos lol jk, lol jk?
-                Agregar( Y.L1, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.L2, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.L3, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.L4, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.R1, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.R2, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.R3, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                Agregar( Y.R4, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
-                return;
-            
-        }
-        public Nodo Fetch(ref Nodo Y, string father)
-        {
-            bool notfound = true;
-            do
-            {
-                if (Y == null) { notfound = false; }
-                if (Y.Valor == father) { return Y; }
                 else
                 {
-                    if (Y.L1.Valor == father) { return Y.L1; }
-                    if (Y.L2.Valor == father) { return Y.L2; }
-                    if (Y.L3.Valor == father) { return Y.L3; }
-                    if (Y.L4.Valor == father) { return Y.L4; }
-                    if (Y.R1.Valor == father) { return Y.R1; }
-                    if (Y.R2.Valor == father) { return Y.R2; }
-                    if (Y.R3.Valor == father) { return Y.R3; }
-                    if (Y.R4.Valor == father) { return Y.R4; }
+                    Agregar(Y.L1, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.L2, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.L3, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.L4, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.R1, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.R2, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.R3, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    Agregar(Y.R4, L1, L2, L3, L4, R1, R2, R3, R4, daddy);
+                    return;
                 }
-
-                Fetch(ref Y.L1, father);
-                Fetch(ref Y.L2, father);
-                Fetch(ref Y.L3, father);
-                Fetch(ref Y.L4, father);
-                Fetch(ref Y.R1, father);
-                Fetch(ref Y.R2, father);
-                Fetch(ref Y.R3, father);
-                Fetch(ref Y.R4, father);
-                return null;
-            } while (notfound);
+            }
+        }
+        public void Fetcher(string final) { Fetch(ref top,final); }
+        public void Fetch(ref Nodo Y, string father)
+        {
+            Contador++;
+            string temp = path;
+            //path = temp + Y.Valor;
+            if (Y == null) { Contador = 0; }
+            else
+            {
+                if (Y.Valor == father) { Console.WriteLine("Recorrido al valor mas largo = {0}\nNiveles ={1} ", path, Contador - 1); return; }
+                else
+                {
+                    Fetch(ref Y.L1, father);
+                    Fetch(ref Y.L2, father);
+                    Fetch(ref Y.L3, father);
+                    Fetch(ref Y.L4, father);
+                    Fetch(ref Y.R1, father);
+                    Fetch(ref Y.R2, father);
+                    Fetch(ref Y.R3, father);
+                    Fetch(ref Y.R4, father);
+                    return;
+                }
+            }
+            
         }
         public void Impresion(ref Nodo Y)
         {
--- Unidad4/E4.Valdes_Fuchs_Agustin/ConsoleApp2/Program.cs
+++ Unidad4/E4.Valdes_Fuchs_Agustin/ConsoleApp2/Program.cs
@@ -14,21 +14,26 @@ namespace ConsoleApp2
             A.Agregarswitch("A",null,null,null,"F",null,null,null,"E");//en este caso el padre es E
             A.Agregarswitch(null, null, null, null, "B", "C","D",null, "A");//papa es A
             A.print();
+            A.Fetcher("B");
+            A.Fetcher("C");
+            A.Fetcher("D");
 
             Arbol B = new Arbol("C");//La raiz es E
             B.Agregarswitch("A", null, null, null, "D", "F", "G", null, "C");//en este caso el padre es C
             B.Agregarswitch(null, null, null, null, "B", null,null, null, "A");//papa es A
             B.Agregarswitch(null, null, null, null, "E", null, null, null, "B");//papa es B
             B.print();
+            B.Fetcher("E");
 
 
             Arbol C = new Arbol("K");//La raiz es E
             C.Agregarswitch("A", "B", "C", "D", null, null, null, null, "K");//en este caso el padre es K
             C.Agregarswitch(null, null, null, null, "E", "I", null, null, "D");//papa es D
-            B.Agregarswitch(null, null, null, null, "F", "G", null, null, "E");//papa es E
-            B.Agregarswitch(null, null, null, null, "J", null, null, null, "I");//papa es I
-            B.Agregarswitch(null, null, null, null, "H", null, null, null, "G");//papa es G
+            C.Agregarswitch(null, null, null, null, "F", "G", null, null, "E");//papa es E
+            C.Agregarswitch(null, null, null, null, "J", null, null, null, "I");//papa es I
+            C.Agregarswitch(null, null, null, null, "H", null, null, null, "G");//papa es G
             C.print();
+            C.Fetcher("H");
             //HAY UN ERROR EN EL METODO DE IMPRESION, REALMENTE CREI QUE FUNCIONARIA 
             //solo que no estoy muy familizarizado con el funionamiento de ref
         }
