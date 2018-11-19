1cd78885d1b4f02134e0782569ba7d984d1ad08b
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2018-11-18 21:27:37 -0800

    Torneotry

---
+++ Unidad4/torneo/ConsoleApp1/Eliminatorias.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Eliminatorias
+    {
+        
+        public Eliminatorias()
+        {
+            Jugador A1= new Jugador("A1");
+            Jugador A2 = new Jugador("A2");
+            Jugador A3 = new Jugador("A3");
+            Jugador A4 = new Jugador("A4");
+            Jugador A5 = new Jugador("A5");
+            Jugador A6 = new Jugador("A6");
+            Jugador A7 = new Jugador("A7");
+            Jugador A8 = new Jugador("A8");
+            Jugador A9 = new Jugador("A9");
+            Jugador A10 = new Jugador("A10");
+            Jugador A11 = new Jugador("A11");
+            Jugador A12 = new Jugador("A12");
+            Jugador B1 = new Jugador("B1");
+            Jugador B2 = new Jugador("B2");
+            Jugador B3 = new Jugador("B3");
+            Jugador B4 = new Jugador("B4");
+            Jugador B5 = new Jugador("B5");
+            Jugador B6 = new Jugador("B6");
+            Jugador B7 = new Jugador("B7");
+            Jugador B8 = new Jugador("B8");
+            Jugador B9 = new Jugador("B9");
+            Jugador B10 = new Jugador("B10");
+            Jugador B11 = new Jugador("B11");
+            Jugador B12 = new Jugador("COMPLEJO B");
+            Jugador LYW = new Jugador("last years winner");
+            
+            
+        }
+    }
+}
---
+++ Unidad4/torneo/ConsoleApp1/Jugador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Jugador
+    {
+        public string Name { get; set; }
+        public Jugador Rival;
+        public Jugador NextMatch;
+        public bool Win = false;
+        public List<int> BoteAbierto;
+        public List<int> BoteCerrado;
+
+        public Jugador(string nombrelocal)
+        {
+            Name = nombrelocal;
+            BoteAbierto = new List<int>();
+            
+            BoteCerrado = new List<int>();
+            BoteCerrado.Add(1);
+        }
+        public void SetRival(Jugador jugadorlocal)
+        {
+            Rival = jugadorlocal;
+        }
+        public void Winnah(List<int> listalocal)
+        {
+            foreach (var item in listalocal)
+            {
+                BoteAbierto.Add(item);
+            }
+            Win = true;
+        }
+    }
+}
