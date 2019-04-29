1467f4161da8ab515a64eb1a3a43b5a794713d8c
dasgrossfuchs <agustinabta@gmail.com>
2019-04-28 17:25:20 -0700

    adfadf

---
+++ CircularLinkedList/CircularLinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircularLinkedList
+{
+    class Node
+    {
+        public Node next { get; set; }
+        public int value { get; set; }
+        public Node previous { get; set; }
+        public Node(int val)
+        {
+            this.value = val;
+            this.next = null;
+            this.previous = null;
+        }
+
+    }
+}
---
+++ CircularLinkedList/CircularLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircularLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList l = new LinkedList();
+            l.Add(1);
+            l.Add(18);
+            l.Add(3);
+            l.Add(4);
+            l.Add(5);
+            l.Delete(45);
+            
+
+        }
+    }
+}
