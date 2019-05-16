5628dd07e77204071328e374a21e94e4ac1418d0
dasgrossfuchs <agustinabta@gmail.com>
2019-05-15 21:30:13 -0700

    omg

---
+++ Treerbol/Tree/Tree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Nodo
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
